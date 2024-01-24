babe95f8edb50662cece9310bd74ce303131a611
Shaik Noor <noorshaik210@gmail.com>
2024-01-24 22:54:16 +0530

    created todo app structure

---
+++ src/app/page.tsx
@@ -0,0 +1,43 @@
+
+
+export default function Home() {
+  return (
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems:'center',
+      flexDirection:"column",
+      border:"1px solid red",
+      height:"100vh"
+    }}>
+      <div>
+        <input type="text" />
+        <button type="button"> OK </button>
+      </div>
+
+      <div style={{
+        display: "flex",
+        flexDirection: "column"
+      }}>
+        <div>
+          <input type="checkbox" name="box1" value="" />
+          <label > go to collage </label>
+        </div>
+
+        <div>
+          <input type="checkbox" name="box1" value="" />
+          <label > go to collage </label>
+        </div>
+        <div>
+          <input type="checkbox" name="box1" value="" />
+          <label > go to collage </label>
+        </div>
+        <div>
+          <input type="checkbox" name="box1" value="" />
+          <label > go to collage </label>
+        </div>
+      </div>
+    </div>
+
+  );
+}
